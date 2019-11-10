6766bbc0ed5d057448acf55923597412b48d8411
Golan Levin <golan@flong.com>
2019-11-10 17:21:48 -0500

    Format tidying

--- unity-arkit/Assets/UnityARKitPlugin/Examples/FeatureDetector/ARAnchorManager.cs
+++ unity-arkit/Assets/UnityARKitPlugin/Examples/FeatureDetector/ARAnchorManager.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 using UnityEngine.XR.iOS;
 
 /* Gets the anchorData's transform value (representing the center of the face), and places an anchor there.*/
-public class ARAnchorManager : MonoBehaviour
-{
+public class ARAnchorManager : MonoBehaviour {
 
     [SerializeField]
     private GameObject anchorPrefab;
-
     private GameObject anchorGo;
-
     private UnityARSessionNativeInterface m_session;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         m_session = UnityARSessionNativeInterface.GetARSessionNativeInterface();
 
         Application.targetFrameRate = 60;
@@ -24,58 +20,47 @@ public class ARAnchorManager : MonoBehaviour
         config.alignment = UnityARAlignment.UnityARAlignmentGravity;
         config.enableLightEstimation = true;
 
-        if (config.IsSupported)
-        {
+        if (config.IsSupported) {
             m_session.RunWithConfig(config);
 
             /* This code subscribes our methods FaceAdded, FaceUpdated, & FaceRemoved to 
-     		* ARKit's Events, meaning our functions will be called when these events occur. */
+             * ARKit's Events, meaning our functions will be called when these events occur. */
             UnityARSessionNativeInterface.ARFaceAnchorAddedEvent += FaceAdded;
             UnityARSessionNativeInterface.ARFaceAnchorUpdatedEvent += FaceUpdated;
             UnityARSessionNativeInterface.ARFaceAnchorRemovedEvent += FaceRemoved;
         }
 
         anchorGo = GameObject.Instantiate(anchorPrefab);
-
     }
 
-    void FaceAdded(ARFaceAnchor anchorData)
-    {
+    void FaceAdded(ARFaceAnchor anchorData) {
         /* anchorData contains the transform position for the center of the face. We set anchorGo's transform to be the same.
-		* Then, we activate the anchor gameobject so it is visible. */
+         * Then, we activate the anchor gameobject so it is visible. */
         anchorGo.transform.position = UnityARMatrixOps.GetPosition(anchorData.transform);
         anchorGo.transform.rotation = UnityARMatrixOps.GetRotation(anchorData.transform);
         anchorGo.SetActive(true);
     }
 
-    void FaceUpdated(ARFaceAnchor anchorData)
-    {
-        if (anchorGo.activeSelf != anchorData.isTracked)
-        {
+    void FaceUpdated(ARFaceAnchor anchorData) {
+        if (anchorGo.activeSelf != anchorData.isTracked) {
             anchorGo.SetActive(anchorData.isTracked);
         }
 
-        if (anchorData.isTracked)
-        {
+        if (anchorData.isTracked) {
             /* anchorData contains the transform position for the center of the face. We set anchorGo's transform to be the same. */
             anchorGo.transform.position = UnityARMatrixOps.GetPosition(anchorData.transform);
             anchorGo.transform.rotation = UnityARMatrixOps.GetRotation(anchorData.transform);
         }
     }
 
-    void FaceRemoved(ARFaceAnchor anchorData)
-    {
+    void FaceRemoved(ARFaceAnchor anchorData) {
         anchorGo.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update() {
     }
 
-    void OnDestroy()
-    {
-
+    void OnDestroy() {
     }
 }
