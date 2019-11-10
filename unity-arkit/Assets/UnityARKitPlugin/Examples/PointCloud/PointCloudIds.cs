390ad17a30a618cd485f6646a780ed329f5eef46
Golan Levin <golan@flong.com>
2019-11-10 17:26:30 -0500

    Format tidying

--- unity-arkit/Assets/UnityARKitPlugin/Examples/PointCloud/PointCloudIds.cs
+++ unity-arkit/Assets/UnityARKitPlugin/Examples/PointCloud/PointCloudIds.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.iOS;
 
-public class PointCloudIds : MonoBehaviour
-{
+public class PointCloudIds : MonoBehaviour {
     bool frameUpdated;
     ulong[] m_PointCloudIdentifiers;
     HashSet<ulong> m_IdentifiersSeenSoFar;
@@ -12,8 +11,7 @@ public class PointCloudIds : MonoBehaviour
 
     private GUIStyle GStyle;
 
-    void Start()
-    {
+    void Start() {
         UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
         frameUpdated = false;
         m_IdentifiersSeenSoFar = new HashSet<ulong>();
@@ -22,38 +20,29 @@ public class PointCloudIds : MonoBehaviour
         GStyle.fontSize = 30;
     }
 
-    void OnGUI()
-    {
+    void OnGUI() {
         int seenThisFrame = (m_PointCloudIdentifiers != null) ? m_PointCloudIdentifiers.Length : 0;
         string formattedMessage = String.Format("{0} new/ {1} frame/ {2} seen", seenThisFrame - m_ExistingIdsSeen, seenThisFrame, m_IdentifiersSeenSoFar.Count);
         GUI.Label(new Rect(100, 100, 200, 40), formattedMessage, GStyle);
     }
 
-    public void ARFrameUpdated(UnityARCamera camera)
-    {
-        if (camera.pointCloud != null)
-        {
+    public void ARFrameUpdated(UnityARCamera camera) {
+        if (camera.pointCloud != null) {
             m_PointCloudIdentifiers = camera.pointCloud.Identifiers;
         }
         frameUpdated = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (frameUpdated)
-        {
+    void Update() {
+        if (frameUpdated) {
             m_ExistingIdsSeen = 0;
-            if (m_PointCloudIdentifiers != null && m_PointCloudIdentifiers.Length > 0)
-            {
-                foreach (var currentPointId in m_PointCloudIdentifiers)
-                {
-                    if (m_IdentifiersSeenSoFar.Contains(currentPointId))
-                    {
+            if (m_PointCloudIdentifiers != null && m_PointCloudIdentifiers.Length > 0) {
+                foreach (var currentPointId in m_PointCloudIdentifiers) {
+                    if (m_IdentifiersSeenSoFar.Contains(currentPointId)) {
                         m_ExistingIdsSeen++;
                     }
-                    else
-                    {
+                    else {
                         m_IdentifiersSeenSoFar.Add(currentPointId);
                     }
                 }
